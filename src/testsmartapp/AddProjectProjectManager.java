51ed241df67fe1f7e2d2b7e41b783a71bc9da98f
nikitasakun <nikitasakun@192.168.88.112>
2024-07-08 20:25:53 +0300

    1.	HR-менеджер открывает систему и должен видеть возможность перейти к списку сотрудников, проектов, запросов на отпуск, запросов на утверждение.
    Имея список сотрудников, менеджер по персоналу может:
    •	сортировка строк таблицы с помощью сортировки в заголовках столбцов,
    •	использовать поиск по имени для строк таблицы;
    •	добавить/обновить/деактивировать сотрудников в списке.
    
    С помощью списка запросов на утверждение менеджер по персоналу может:
    •	сортировка строк таблицы с помощью сортировки в заголовках столбцов,
    •	использовать поиск по номеру запроса для строк таблицы;
    •	открыть запрос - открывается новая форма с подробностями.
    •	утвердить запрос на соответствующий отпуск, который обновляется с учетом соответствующего статуса, и перерасчет остатка отсутствия сотрудников;
    
    •	отклонить запрос - соответствующий запрос на отпуск обновляется соответствующим статусом. Напишите комментарий, объясняющий, что отказ должен быть возможен.
    
    
    
    С помощью списка просьб об отпусках руководитель кадровой службы может:
    •	сортировка строк таблицы с помощью сортировки в заголовках столбцов,
    •	использовать поиск по номеру запроса для строк таблицы;
    •	открыть запрос - открывается новая форма с подробностями.
    
    С помощью списка проектов менеджер по персоналу может:
    •	сортировка строк таблицы с помощью сортировки в заголовках столбцов,
    •	использовать поиск по номеру запроса для строк таблицы;
    •	открыть проект- открывается новая форма с подробностями.
    
    2.	Менеджер проекта открывает систему и должен видеть возможность перейти к списку сотрудников, проектов, запросов на отпуск, запросов на утверждение.
    Имея список сотрудников, руководитель проекта может:
    •	сортировка строк таблицы с помощью сортировки в заголовках столбцов,
    •	использовать поиск по имени для строк таблицы; – (**)
    •	Открыть сотрудника - открывается новая форма с подробностями.
    •	назначить сотрудника для проектов.
    С помощью списка проектов руководитель проекта может:
    •	сортировка строк таблицы с помощью сортировки в заголовках столбцов,
    •	использовать поиск по номеру запроса для строк таблицы; – (**)
    •	добавить/обновить/деактивировать проекты в списке.

---
+++ src/testsmartapp/AddProjectProjectManager.java
@@ -0,0 +1,140 @@
+package testsmartapp;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.sql.*;
+
+public class AddProjectProjectManager extends JFrame implements ActionListener{
+    
+    JTextField textfstatus, textfenddate, textftype, textfstartdate, textfcomment;
+    JButton add, back;
+    Choice chtype;
+    AddProjectProjectManager() {
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        JLabel heading = new JLabel("Update Project Detail");
+        heading.setBounds(320, 30, 500, 50);
+        heading.setFont(new Font("SAN_SERIF", Font.BOLD, 25));
+        add(heading);
+        
+        JLabel labeltype = new JLabel("Project Type");
+        labeltype.setBounds(400, 200, 150, 30);
+        labeltype.setFont(new Font("serif", Font.PLAIN, 20));
+        add(labeltype);
+        
+        chtype = new Choice();
+        chtype.setBounds(600, 200, 150, 30);
+        add(chtype);
+        
+        try {
+            ConnectionDB c = new ConnectionDB();
+            ResultSet rs = c.s.executeQuery("SELECT * FROM public.projecttype ORDER BY id");
+            while(rs.next()){
+                chtype.add(rs.getString("typename"));
+            }
+            
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        
+        JLabel labelstartdate = new JLabel("Start Date");
+        labelstartdate.setBounds(50, 250, 150, 30);
+        labelstartdate.setFont(new Font("serif", Font.PLAIN, 20));
+        add(labelstartdate);
+        
+        textfstartdate = new JTextField();
+        textfstartdate.setBounds(200, 250, 150, 30);
+        add(textfstartdate);
+        
+        JLabel labelenddate = new JLabel("End Date");
+        labelenddate.setBounds(400, 250, 150, 30);
+        labelenddate.setFont(new Font("serif", Font.PLAIN, 20));
+        add(labelenddate);
+        
+        textfenddate = new JTextField();
+        textfenddate.setBounds(600, 250, 150, 30);
+        add(textfenddate);
+       
+        
+        JLabel labelcomment = new JLabel("Comment");
+        labelcomment.setBounds(50, 300, 150, 30);
+        labelcomment.setFont(new Font("serif", Font.PLAIN, 20));
+        add(labelcomment);
+        
+        textfcomment = new JTextField();
+        textfcomment.setBounds(200, 300, 150, 30);
+        add(textfcomment);
+        
+        JLabel labelstatus = new JLabel("Status(t/f)");
+        labelstatus.setBounds(400, 300, 150, 30);
+        labelstatus.setFont(new Font("serif", Font.PLAIN, 20));
+        add(labelstatus);
+        
+        textfstatus = new JTextField();
+        textfstatus.setBounds(600, 300, 150, 30);
+        add(textfstatus);
+        
+        add = new JButton("Add");
+        add.setBounds(250, 550, 150, 40);
+        add.addActionListener(this);
+        add.setBackground(Color.BLACK);
+        add.setForeground(Color.WHITE);
+        add(add);
+        
+        back = new JButton("Back");
+        back.setBounds(450, 550, 150, 40);
+        back.addActionListener(this);
+        back.setBackground(Color.BLACK);
+        back.setForeground(Color.WHITE);
+        add(back);
+        
+        setSize(900, 700);
+        setLocation(300, 50);
+        setVisible(true);
+    }
+    
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == add) {
+            String startdate = textfstartdate.getText();
+            String status = textfstatus.getText();
+            
+            String query1 = "SELECT * FROM public.projecttype WHERE typename = '"+chtype.getSelectedItem()+"'";
+            String typeId = "";
+            try {
+                ConnectionDB c = new ConnectionDB();
+                ResultSet rs = c.s.executeQuery(query1);
+                while(rs.next()){
+                typeId = rs.getString("id");
+                
+                }
+            }
+            catch (Exception e) {
+            e.printStackTrace();
+            }
+            
+            int type = Integer.parseInt(typeId);
+            String enddate = textfenddate.getText();
+            String comment = textfcomment.getText();
+            
+            try {
+                ConnectionDB conn = new ConnectionDB();
+                String query = "INSERT INTO public.project (projecttypeid, startdate, enddate, projectmanagerid, comment, status) VALUES ('"+type+"', '"+startdate+"', '"+enddate+"', '"+Account.id+"', '"+comment+"', '"+status+"')";
+                conn.s.executeUpdate(query);
+                JOptionPane.showMessageDialog(null, "Project add successfully");
+                setVisible(false);
+                new ViewProjectsProjectManager();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            setVisible(false);
+            new ViewProjectsProjectManager();
+        }
+    }
+
+    public static void main(String[] args) {
+        new AddProjectProjectManager();
+    }
+}
